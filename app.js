f9960bd5291928222df8418901043e5c1551074c
Anna Kulibabina <anna.kulibabina@gmail.com>
2022-08-06 16:47:06 +0300

    update hw

--- app.js
+++ app.js
@@ -1,12 +1,5 @@
-// var with yargs:
-// const yargs = require("yargs");
-// const {hideBin} = require("yargs/helpers");
-// ----------------------------------
-
-
 const { Command } = require("commander");
 const program = new Command();
-// program.version("9.4.0")
 
 const contactsOperations = require("./db");
 
@@ -25,7 +18,6 @@ const invokeAction = async({action, id, name, email, phone})=> {
           break;
         case "add":
           const newContact = await contactsOperations.add(name, email, phone);
-          //   console.log(newContact);
           break;
         case "updateById":
           const updateContact = await contactsOperations.updateById(id, name, email, phone);
@@ -55,17 +47,5 @@ program
 program.parse(process.argv);
 
 const argv = program.opts();
-// console.log(argv);
 
 invokeAction(argv);
-
-// ---------------------------------
-// var with yargs:
-// const arr = hideBin(process.argv);
-
-// const {argv} = yargs(arr);
-
-//  or var 2 with slise, not use hideBin
-// const {argv} = yargs(process.argv.slice(2));
-
-// invokeAction(argv);
